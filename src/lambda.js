9b99f22e7cb11d3e277155f2488d87cf0d81f574
Dave Dean <david.dean@hackney.gov.uk>
2023-04-11 15:19:28 +0100

    lambda use src

--- src/lambda.js
+++ src/lambda.js
@@ -3,7 +3,6 @@ const server = require('restana')()
 const files = require('serve-static')
 const path = require('path')
 
-server.use(files(path.join(__dirname, 'build')))
-server.use(files(path.join(__dirname, 'public')))
+server.use(files(path.join(__dirname, 'src')))
 
 module.exports.handler = require('serverless-http')(server)
