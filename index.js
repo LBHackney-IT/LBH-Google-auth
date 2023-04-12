2bcf2ef39ebd4236530a84b0dad1609f1dea4897
Dave Dean <david.dean@hackney.gov.uk>
2023-04-12 12:24:40 +0100

    Add status

--- index.js
+++ index.js
@@ -23,5 +23,9 @@ const adminOAuth2Client = new OAuth2Client(
 app.get('/', function (req, res) {
   res.send('Hello World!' + process.env.OAUTH_CLIENT_ID)
 })
+
+app.get('/status', (req, res) => {
+    res.send('Google LBH Auth is running on an AWS Lambda!')
+})
  
 module.exports.handler = serverless(app);
