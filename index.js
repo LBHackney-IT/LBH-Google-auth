acb8825d87bcc233e115eadbb01adbb11c0cb744
Dave Dean <david.dean@hackney.gov.uk>
2023-04-12 12:11:03 +0100

    Rebuild OAuth2client

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
  
+const {OAuth2Client} = require('google-auth-library');
+
+const userOAuth2Client = new OAuth2Client(
+    process.env.OAUTH_CLIENT_ID,
+    process.env.OAUTH_CLIENT_SECRET,
+    process.env.OAUTH_CALLBACK_URL
+  );
+  
+const adminOAuth2Client = new OAuth2Client(
+    process.env.OAUTH_CLIENT_ID,
+    process.env.OAUTH_CLIENT_SECRET,
+    process.env.ADMIN_OAUTH_CALLBACK_URL
+  );
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World!' + process.env.OAUTH_CLIENT_ID)
 })
  
 module.exports.handler = serverless(app);
